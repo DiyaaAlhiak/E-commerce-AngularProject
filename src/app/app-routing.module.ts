f882f7a7c055a08d4d327116a521fd1c016dfa39
DiyaaAlhiak <diyaahaik77@gmail.com>
2024-10-20 19:47:12 +0300

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllProductComponent } from './products/components/all-product/all-product.component';
+import { ProductDetailsComponent } from './products/components/product-details/product-details.component';
+import { CartComponent } from './carts/components/cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:"Product",
+  component:AllProductComponent,
+},
+{
+  path:"Product-detils",
+  component:ProductDetailsComponent,
+},
+{
+  path:"cart",
+  component:CartComponent,
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
